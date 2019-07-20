16a8dcc971c251d4ae1cbae9778914986142c389
edvaldoJr3 <jGuerra98r>
2019-07-19 21:21:26 -0300

    Aula 6 concluída.

--- forum/src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDto.java
+++ forum/src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDto.java
@@ -1,27 +1,33 @@
 package br.com.alura.forum.controller.dto;
 
+import br.com.alura.forum.model.StatusTopico;
 import br.com.alura.forum.model.Topico;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TopicoDTO {
+public class DetalhesDoTopicoDto {
 
     private Long id;
     private String titulo;
     private String mensagem;
     private LocalDateTime dataCriacao;
+    private String nomeAutor;
+    private StatusTopico status;
+    private List<RespostaDto> respostas;
 
-    public TopicoDTO(Topico topico){
+    public DetalhesDoTopicoDto(Topico topico){
         this.id = topico.getId();
         this.titulo = topico.getTitulo();
         this.mensagem = topico.getMensagem();
         this.dataCriacao = topico.getDataCriacao();
-    }
+        this.nomeAutor = topico.getAutor().getNome();
+        this.status = topico.getStatus();
 
-    public static List<TopicoDTO> converter(List<Topico> topicos) {
-        return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+        this.respostas = new ArrayList<>();
+        this.respostas.addAll(topico.getRespostas().stream().map(RespostaDto::new).collect(Collectors.toList()));
     }
 
     public Long getId() {
@@ -38,5 +44,17 @@ public class TopicoDTO {
 
     public LocalDateTime getDataCriacao() {
         return dataCriacao;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public StatusTopico getStatus() {
+        return status;
+    }
+
+    public List<RespostaDto> getRespostas() {
+        return respostas;
     }
 }
